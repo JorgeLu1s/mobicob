e83c379d83f23fc88c721c8fc42defe4969c0438
90miguelangel <90miguelangel@gmail.com>
2018-07-29 20:24:58 -0500

    added fields for edition and creation

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,26 +1,49 @@
 ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
+  controller do
+    def action_methods
+      super - ['destroy']
+    end
+  end
+
   index do
     selectable_column
     id_column
+    column :id_number
+    column :name
+    column :lastname
+    column :position
+    column :delegation
+    column :role
+    column :phone
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :active
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+  filter :position
+  filter :delegation
+  filter :role
+  filter :active
 
   form do |f|
+    f.semantic_errors *f.object.errors.keys
     f.inputs do
+      f.input :id_number
+      f.input :name
+      f.input :lastname
+      f.input :position
+      f.input :delegation
+      f.input :role
+      f.input :phone
+      f.input :address
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :active
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
     end
     f.actions
   end
