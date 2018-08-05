61d228616a95a5be77895fca757a6c88816e6d30
Jorgelu1s <mejiajimenez@gmail.com>
2018-08-05 10:10:08 -0500

    Set up the root to ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'admin/dashboard#index'
+
   namespace :v1, defaults: { format: 'json' } do
     devise_scope :user do
       post '/sign_in', to: 'sessions#create'
