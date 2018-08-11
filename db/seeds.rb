101b98c652c76d8adc6a9d4163ccd0f02e2c5764
90miguelangel <90miguelangel@gmail.com>
2018-08-11 18:59:21 -0500

    added delegation, contractor and role to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+delegation = Delegation.where(code: "1068", name: "Atl. Nte").first_or_create
+contractor = Contractor.where(code: "0", name: "Contratista Generico").first_or_create
+role = Role.where(code: "1", name: "Administrador").first_or_create
+user = User.where(email: 'admin@example.com', password: 'password',
+  role: role, contractor: contractor,
+  delegation: delegation).first_or_create #if Rails.env.development?
